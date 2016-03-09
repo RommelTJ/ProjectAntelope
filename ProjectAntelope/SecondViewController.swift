40715e44399c118373a8213d7a9938c216b80bd7
Rommel Rico <rommeltj@gmail.com>
2016-03-09 13:51:23 -0800

    Initial Commit

---
+++ ProjectAntelope/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ProjectAntelope
+//
+//  Created by Rommel Rico on 3/9/16.
+//  Copyright © 2016 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
